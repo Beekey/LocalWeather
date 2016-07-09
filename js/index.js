bc222b993e9dee57181de7533b7aa68eee7b8a41
Beekey <bikkidesigns@gmail.com>
2016-07-09 13:58:53 +0200

    Commented out code
    commented out code that previously reacted if protocol was https:

--- js/index.js
+++ js/index.js
@@ -3,9 +3,9 @@
 // it can be solved by using a remote CORS server
 // but I found them to be down a lot, so I opted
 // for this solution
-if (window.location.protocol === "https:") {
+/* if (window.location.protocol === "https:") {
   $(".container-fluid").html("Please replace the https in the address bar url to http. Thank you :)")
-};
+}; */
 
 $(document).ready(function() {
   $("img").addClass("img-responsive");
